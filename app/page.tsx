3bfd37e13bd1ab01150260e762d4d01f0ae65387
renyu0 <rdaily447@gmail.com>
2024-11-10 10:35:17 +0900

    dd

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <p>
           안녕!<br/>
           여긴 내가 만들고 관리하는<br/>
-          어쩌고 저쩌고 하는 웹 사이트야!
+          어쩌고 저쩌고 하는 웹 사이트야
           </p>
       </div>
       
